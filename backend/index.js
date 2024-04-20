1389d8298028ad0460e7354981b794ac1c3e3cc3
Adrien Russo <adrien@appro-auto.com>
2024-04-20 13:34:08 +0200

    Initialisation du projet

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// Load environment variables from .env file
+require("dotenv").config();
+
+// Import the Express application from src/app.js
+const app = require("./src/app");
+
+// Get the port from the environment variables
+const port = process.env.BACKEND_PORT;
+// Start the server and listen on the specified port
+app
+    .listen(port, () => {
+        console.info(`Server is listening on port ${port}`);
+    })
+    .on("error", (err) => {
+        console.error("Error:", err.message);
+    });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+
+function App() {
+    const [helloMessage, setHelloMessage] = useState('')
+    return <h1>Hello from Russo-CORP.</h1>
+}
+
+export default App
